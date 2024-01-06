3970a97f364019401c6551c966da9b3c64606b1a
Fizic <kirya.firsov.04@gmail.com>
2024-01-06 19:19:38 +0500

    feat: pretty loader and title

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import React, {useEffect, useState} from "react";
-import {Button, Card} from '@gravity-ui/uikit';
-
-import Draggable from 'react-draggable';
+import {Button, Card, Text, Loader} from '@gravity-ui/uikit';
 
 
 export interface Buyer {
@@ -14,7 +12,6 @@ export interface Buyer {
 const Main = () => {
     const [buyers, setBuyers] = useState<Buyer[]>([])
     const [buyerId, setBuyerId] = useState<number>(0)
-    const [coords, setCoords] = useState({x: 0, y: 0});
     useEffect(() => {
         if (buyers.length > 0) return
         axios.get('https://toadabroad.kfirsov.com/api/buyers').then(({data: buyers}: { data: { buyers: Buyer[] } }) => {
@@ -24,37 +21,20 @@ const Main = () => {
         })
     })
 
-    const trackMouse = (event: any) => {
-        setCoords({
-            x: event.clientX,
-            y: event.clientY,
-        });
-    };
-
     const onclk = (event: any) => {
         setBuyerId((buyerId + 1) % buyers.length);
     };
 
-    const startDrag = (e: any) => {
-        window.addEventListener('mousemove', trackMouse);
-        console.log('start')
-    }
-    const stopDrag = (e: any) => {
-        window.removeEventListener('mousemove', trackMouse);
-        setCoords({
-            x: 0,
-            y: 0,
-
-        })
-        console.log('stop')
-    }
-
-
-    if (buyers.length === 0) return <div>Loading...</div>
+    if (buyers.length === 0) return <div className="h-dvh flex justify-center items-center"><Loader size="m" /></div>
 
     return (
         <div className="h-dvh flex justify-center items-center">
             <div className="flex flex-col gap-10">
+                <div className="flex justify-center items-center gap-10">
+                    <img className="rounded-t-[7px] max-w-20"
+                         src="./logo512.jpg" alt="" draggable={false}/>
+                    <Text variant="display-3">Toad Abroad</Text>
+                </div>
                 <div className="flex justify-center items-center">
                     <div className="handle">
                         <Card className="text-center">
